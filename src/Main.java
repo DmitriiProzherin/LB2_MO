63c5b4fd6dff59cfaff0087270fdb3f97fa78d73
BehemothDi <dmitriiprozherin@mail.ru>
2022-12-27 06:35:12 +0300

    -

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
         HalfDivisionSearch search = new HalfDivisionSearch();
-
         double min = search.naive(0, 1, 0.4);
-
-       // System.out.printf("Минимальное значение функции: %.12f", min);
     }
 }
